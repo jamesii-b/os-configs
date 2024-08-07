77b0867444fb7056a878f7a62d76fe5c72b60f71
james bhattarai <jamesbhattarai14@gmail.com>
2024-08-07 09:23:04 +0545

    Chore: overwrite main.tf with openstack kucloud

--- main.tf
+++ main.tf
@@ -1,25 +1,30 @@
 terraform {
   required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 3.0.1"
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 2.0"
     }
   }
 }
 
-provider "docker" {}
-
-resource "docker_image" "nginx" {
-  name         = "nginx"
-  keep_locally = false
+provider "openstack" {
+  auth_url           = "http://192.168.3.254:5000"
+  user_name          = "admin"
+  password           = "notapasswordGoAWAY"
+  tenant_name        = "admin"
+  region             = "RegionOne"
+  domain_name        = "Default"
 }
 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  name  = "tutorial"
-
-  ports {
-    internal = 80
-    external = 8000
+resource "openstack_compute_instance_v2" "server" {
+  name            = "tf-instance"
+  image_id         = "c3b24cd0-88ff-4c25-b654-6b17e059be55"  # Ubuntu 24.04 image
+  flavor_id        = "70fdc265-a080-4b45-b569-038e75778893"  # t1.mini flavor
+  network {
+    uuid = "7b8cd7c7-f9d3-4295-918a-687127c96f29"  # default network
   }
+}
+
+output "instance_ip" {
+  value = openstack_compute_instance_v2.server.access_ip_v4
 }
